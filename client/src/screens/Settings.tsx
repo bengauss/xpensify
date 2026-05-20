f0ecf130170343c733198663c5cfe47a0775bc56
bengauss <bengau.at@gmail.com>
2026-05-20 22:35:44 +0200

    bump: version to v3.02

--- client/src/screens/Settings.tsx
+++ client/src/screens/Settings.tsx
@@ -1071,7 +1071,7 @@ function AboutSection() {
     <Section title="about">
       <Row>
         <span class="flex-1 text-sm">version</span>
-        <span class="text-sm" style={{ color: "var(--color-text-secondary)" }}>v3.0</span>
+        <span class="text-sm" style={{ color: "var(--color-text-secondary)" }}>v3.02</span>
       </Row>
       {confirmLogout ? (
         <div class="px-4 py-2">
